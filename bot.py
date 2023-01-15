cb6aac936a41b9cc37b0de8da4928f95d952c97c
sceinja <adilrabbani48@gmail.com>
2023-01-15 19:24:09 +0500

    added 'check for verification', deleted unnecessary lines

--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
 import discord
 from discord.ext import commands
+from discord import app_commands
+from discord.utils import get
 from dotenv import load_dotenv
 import os
 
@@ -7,36 +9,39 @@ import os
 load_dotenv()
 token = os.getenv('TOKEN')
 
-#sets up bot intents
-intents=discord.Intents.default()
-intents = intents.all()
-
 #sets up bot object
-client = commands.Bot(command_prefix='$', intents=intents)
+client = commands.Bot(command_prefix='$', intents= discord.Intents.all())
 
 @client.event
 async def on_ready():
     print(f"{client.user} is on")
 
-@client.command(name='verify', description='grants access to the server')
-async def verify(ctx, user: discord.User):
+@client.command(description='grants access to the server')
+async def verify(ctx, user: discord.Member):
 
     if ctx.author.guild_permissions.manage_roles:
-
-        user_object = user
-        guild = ctx.guild
-        member = ctx.guild.get_member(user_object.id)
-
-        add_role = discord.utils.get(guild.roles, name='fish')
-        remove_role = discord.utils.get(guild.roles, name='washups')
-
-        await member.add_roles(add_role)
-        await member.remove_roles(remove_role)
-
-        await ctx.send(f"{user_object} has been granted access to {guild.name}")
     
+        guild = ctx.guild
+        member = user
+    
+        role = get(guild.roles, name='fish')
+    
+        if role in member.roles:
+    
+            await ctx.send(f"{member.mention} is already verified")
+    
+        else:
+    
+            add_role = discord.utils.get(guild.roles, name='fish')
+            remove_role = discord.utils.get(guild.roles, name='washups')
+    
+            await member.add_roles(add_role)
+            await member.remove_roles(remove_role)
+    
+            await ctx.send(f"{user.mention} has been granted access to {guild.name}")
     else:
         
         await ctx.send("You don't have the 'manage roles' permission")
+
 
 client.run(token)
