905199555356be5f68e59c3799309e9862cce98e
sceinja <adilrabbani48@gmail.com>
2023-01-15 17:32:23 +0500

    fixed permission bug

--- bot.py
+++ bot.py
@@ -5,10 +5,13 @@ import os
 
 #loads token from .env file
 load_dotenv()
-token = os.getenv('token_v')
+token = os.getenv('TOKEN')
 
+#sets up bot intents
 intents=discord.Intents.default()
 intents = intents.all()
+
+#sets up bot object
 client = commands.Bot(command_prefix='$', intents=intents)
 
 @client.event
@@ -17,16 +20,23 @@ async def on_ready():
 
 @client.command(name='verify', description='grants access to the server')
 async def verify(ctx, user: discord.User):
-    user_object = user
-    guild = ctx.guild
-    member = ctx.guild.get_member(user_object.id)
 
-    add_role = discord.utils.get(guild.roles, name='fish')
-    remove_role = discord.utils.get(guild.roles, name='washups')
+    if ctx.author.guild_permissions.manage_roles:
 
-    await member.add_roles(add_role)
-    await member.remove_roles(remove_role)
+        user_object = user
+        guild = ctx.guild
+        member = ctx.guild.get_member(user_object.id)
 
-    await ctx.send(f"The nigger: {user_object} has been granted access to the cotton fields!")
+        add_role = discord.utils.get(guild.roles, name='fish')
+        remove_role = discord.utils.get(guild.roles, name='washups')
+
+        await member.add_roles(add_role)
+        await member.remove_roles(remove_role)
+
+        await ctx.send(f"{user_object} has been granted access to {guild.name}")
+    
+    else:
+        
+        await ctx.send("You don't have the 'manage roles' permission")
 
 client.run(token)
