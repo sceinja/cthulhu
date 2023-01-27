82ba9d7248acc35438f8aa1089e1dc5f8d5ddb49
sceinja <adilrabbani48@gmail.com>
2023-01-27 23:54:39 +0500

    fixed unban command

--- commands/unban.py
+++ commands/unban.py
@@ -1,24 +1,21 @@
-import discord
-from discord.ext import commands
 
+from discord.ext import commands
 
 class unban(commands.Cog):
     def __init__(self, client):
         self.client = client
 
     @commands.hybrid_command(name='unban', description='unbans a banned user', with_app_command=True)
-    async def unban(self, ctx: commands.Context, userid: str = None):
+    async def unban(self, ctx: commands.Context, userid):
         
         if ctx.author.guild_permissions.administrator:
-
 
             if userid==None:
                 await ctx.send('Please specify a userid to be unbanned')
 
             else:
-                user = ctx.guild.get_member(int(userid))
-                await ctx.guild.unban(user)
-                await ctx.send(f"`{user}` has been unbanned!")
+                await ctx.guild.unban(userid)
+                await ctx.send(f"`{userid}` has been unbanned!")
 
         else:
             await ctx.send("You don't have valid permissions")
